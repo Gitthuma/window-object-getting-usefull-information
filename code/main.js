04ea9fdae13a5936838853ccbb21c7b092e56778
Gitthuma <george.githuma@gmail.com>
2021-04-18 18:48:56 +0300

    Dynamically altered the width and height of the div to equal that of the viewport.

--- code/main.js
+++ code/main.js
@@ -1,5 +1,10 @@
-/*First of all, let's grab a reference to the div, and then grab the width and height of the viewport (the inner window, where your document is displayed) and store them in variables — these two values are handily contained in the Window.innerWidth and Window.innerHeight properties. */
+/*Grab a reference to the div, and then grab the width and height of the viewport (the inner window, where your document is displayed) and store them in variables — these two values are handily contained in the Window.innerWidth and Window.innerHeight properties. */
 
 const div = document.querySelector('div');
 let winWidth = window.innerWidth;
 let winHeight =window.innerHeight;
+
+/*Dynamically alter the width and height of the div to equal that of the viewport.*/
+
+div.style.width = winWidth + 'px';
+div.style.height = winHeight + 'px';
